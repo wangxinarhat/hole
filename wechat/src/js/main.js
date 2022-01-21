0a67faacc2eeadcae1181ef3b755b17658ede4a7
Kaka <wangxinarhat@gmail.com>
2022-01-21 17:36:13 +0800

    timeline praise final

--- wechat/src/js/main.js
+++ wechat/src/js/main.js
@@ -48,6 +48,7 @@ function timeline(praiseCount) {
         let praisePopSelector = id("com.tencent.mm:id/ng").desc("评论").clz("android.widget.ImageView");
         let praisePopNode = praisePopSelector.getOneNodeInfo(1000);
 
+        logi("praisePopNode = " + praisePopNode);
         //找不到pop入口，Y轴坐标太小、太大则滑动1/6屏幕，跳过
         if (!praisePopNode || praisePopNode.bounds.top < SCREEN_HEIGHT / 6 || praisePopNode.bounds.bottom > SCREEN_HEIGHT / 6 * 5) {
             logi("找不到pop入口，Y轴坐标太小、太大则滑动 1/8 屏幕，跳过 ！！！ ");
@@ -86,7 +87,7 @@ function timeline(praiseCount) {
                 swipeAndSleep(SCREEN_HEIGHT / 8);
             } else {
                 let firstColor = "#4C4C4C-#101010";
-                let multiColor = "-42|-36|#DDDDDD-#101010,-99|-47|#4C4C4C-#101010,-114|-56|#4C4C4C-#101010,-114|2|#4C4C4C-#101010,-46|-29|#AAAAAA-#101010,-36|5|#4C4C4C-#101010,-5|1|#4C4C4C-#101010,-52|-9|#4C4C4C-#101010,-8|3|#4C4C4C-#101010,-49|-5|#4D4D4D-#101010,-81|-46|#4C4C4C-#101010,-24|0|#4C4C4C-#101010,-13|-40|#4C4C4C-#101010,-119|-22|#4C4C4C-#101010,-124|-5|#4C4C4C-#101010,-25|-33|#FFFFFF-#101010,-123|0|#4C4C4C-#101010,-49|-26|#4C4C4C-#101010,-15|-20|#4C4C4C-#101010,-83|19|#4C4C4C-#101010,-88|-1|#A6A6A6-#101010,-58|-1|#4C4C4C-#101010,-2|-43|#4C4C4C-#101010,-34|-36|#ABABAB-#101010,-14|-24|#4C4C4C-#101010,-72|-45|#4C4C4C-#101010,-24|-2|#4C4C4C-#101010,-75|-57|#4C4C4C-#101010,-53|-28|#4C4C4C-#101010";
+                let multiColor = "-61|-29|#4C4C4C-#101010,-83|-47|#4C4C4C-#101010,-86|-18|#CECECE-#101010,-10|-44|#4C4C4C-#101010,-18|-30|#FFFFFF-#101010,-57|2|#4C4C4C-#101010,-56|-17|#FFFFFF-#101010,0|-43|#4C4C4C-#101010,-66|-2|#A6A6A6-#101010,-21|-28|#FFFFFF-#101010,-7|-29|#FFFFFF-#101010,-51|0|#4C4C4C-#101010,-10|-39|#4C4C4C-#101010,2|-8|#4C4C4C-#101010,-23|-30|#FFFFFF-#101010,-75|-23|#4C4C4C-#101010,-20|-33|#4C4C4C-#101010,-78|-31|#4C4C4C-#101010,-86|-7|#4C4C4C-#101010,-22|-4|#828282-#101010,-12|-13|#4C4C4C-#101010,-73|2|#4C4C4C-#101010,-50|-2|#4C4C4C-#101010,-21|-4|#4C4C4C-#101010,2|-46|#4C4C4C-#101010,-42|2|#4C4C4C-#101010,-51|-26|#FFFFFF-#101010,-45|-23|#4C4C4C-#101010,-30|-18|#4C4C4C-#101010";
                 let points = image.findMultiColor(tmpImage, firstColor, multiColor, 0.9, 0, 0, 0, 0, 1, 1);
                 //这玩意是个数组
                 if (points && points.length > 0) {
