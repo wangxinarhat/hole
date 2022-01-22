20aada6f864cfe67685af54390b991b692450c76
Kaka <wangxinarhat@gmail.com>
2022-01-22 13:58:34 +0800

    get nickname

--- wechat/src/js/main.js
+++ wechat/src/js/main.js
@@ -2,26 +2,56 @@ const SCREEN_WIDTH = device.getScreenWidth();
 const SCREEN_HEIGHT = device.getScreenHeight();
 
 function main() {
+    logd("开始执行脚本------>")
+    let isPraiseParam;
+    let isPraise = readConfigString("praise_timeline");
+    let isMessage = readConfigString("bless_message");
+    let messageContent = readConfigString("message_content");
+    let praiseCount = readConfigInt("praise_count");
+    logd("isPraise : " + isPraise + " ; type = " + typeof isPraise);
+    logd("isMessage : " + isMessage + " ; type = " + typeof isMessage);
+    logd("messageContent: " + messageContent + " ; type = " + typeof messageContent);
+    logd("praiseCount: " + praiseCount + " ; type = " + typeof praiseCount);
+
+    isPraiseParam = false;
+    if (isPraiseParam) {
+
+        // if ("ture" === readConfigString("praise_timeline")) {
+        logi("是点赞 ！！！ ");
+        isPraiseParam = true;
+        if (praiseCount < 100 && praiseCount > 1000) {
+            toast("点赞数量应该100～1000");
+            return;
+        }
+    } else {
+        logi("发消息 ！！！ ");
+        isPraiseParam = false;
+        // if ("" === readConfigString("message_content")) {
+        //     toast("请填入消息内容～");
+        //     return;
+        // }
+    }
     if (!commonUtils.autoServiceStart(3)) {
         logd("自动化服务启动失败，无法执行脚本")
-        exit();
+        // exit();
         return;
     }
-
-
-    logd("praise : " + readConfigString("praise_timeline"));
-    logd("message : " + readConfigString("bless_message"));
-    logd("content: " + readConfigString("message_content"));
-
-
-    logd("开始执行脚本------>")
-    utils.openAppByName("微信");
+    let startRet = utils.openAppByName("微信");
+    logd("启动微信结果 ： " + startRet);
+    if (!startRet) {
+        toast("启动微信失败，检查手机是否安装微信app");
+        return;
+    }
     sleep(5000);
-    let praiseCount = readConfigInt("praise_count");
-    logd("点赞个数 ： " + praiseCount);
-    timeline(praiseCount);
-    logd("脚本执行完毕------>")
-    exit();
+    if (isPraiseParam) {
+        timeline(praiseCount);
+    } else {
+        messageContent = "test~";
+        send(messageContent);
+    }
+
+    logd("脚本执行完毕------>");
+    // exit();
 }
 
 main();
